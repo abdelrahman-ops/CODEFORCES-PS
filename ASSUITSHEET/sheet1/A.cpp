f2daa62dab2f58f9a3e70647fcad0cb85366f9c5
Abdelrahman Ataa <72919098+abdelrahman-ops@users.noreply.github.com>
2023-05-16 22:56:31 +0300

    Problem Solving
    Problem Solving

--- ASSUITSHEET/sheet1/A.cpp
+++ ASSUITSHEET/sheet1/A.cpp
@@ -3,7 +3,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     string name;
     cin >> name;
     cout << "Hello, " << name << endl;
---
+++ ASSUITSHEET/sheet1/B.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+//int, long long, char, float and double
+int main() {
+   int n;
+   long long num ;
+   char c ;
+   float f ;
+   double d ;
+   cin >> n >> num >> c >> f >> d ;
+   cout << n <<endl;
+   cout << num <<endl;
+   cout << c <<endl;
+   cout << f <<endl;
+   cout << d <<endl;
+    return 0;
+}
